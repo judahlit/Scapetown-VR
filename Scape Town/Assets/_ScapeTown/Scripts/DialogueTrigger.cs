d4e7cbc980306e582cb27ba67511f95adc7c6b10
Judah <i460958@fhict.nl>
2022-05-24 22:55:42 +0200

    Improved dialogue trigger code

--- Scape Town/Assets/_ScapeTown/Scripts/DialogueTrigger.cs
+++ Scape Town/Assets/_ScapeTown/Scripts/DialogueTrigger.cs
@@ -7,24 +7,51 @@ using UnityEngine.UI;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public Button buttonTalk;
     public GameObject player;
+    public GameObject dialoguePanel;
     public GameObject textName;
     public GameObject textDialogue;
 
     private float distance;
+    private bool buttonTalkIsActive;
+
+    private void Start()
+    {
+        SetButtonTalk(false);
+        dialoguePanel.SetActive(false);
+    }
 
     private void Update()
     {
         distance = Vector3.Distance(transform.position, player.transform.position);
 
-        if (distance < 2f)
+        if (distance <= 2f && !buttonTalkIsActive)
         {
-            TriggerDialogue();
+            SetButtonTalk(true);
+        }
+
+        if (distance > 2f && buttonTalkIsActive)
+        {
+            SetButtonTalk(false);
+        }
+
+        if (buttonTalk.IsActive())
+        {
+            buttonTalk.onClick.AddListener(TriggerDialogue);
         }
     }
 
     public void TriggerDialogue()
     {
+        SetButtonTalk(false);
+        dialoguePanel.SetActive(true);
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue, textName, textDialogue);
+    }
+
+    private void SetButtonTalk(bool active)
+    {
+        buttonTalk.gameObject.SetActive(active);
+        buttonTalkIsActive = active;
     }
 }
