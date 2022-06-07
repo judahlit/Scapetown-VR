928871ef22786f288e1bc0c00fa4d8ba8ea8b13c
Krystian Maraszek <28630895+Gryfin123@users.noreply.github.com>
2022-06-07 13:05:58 +0200

    Prepared vignette effect for movmeent, but not implemented yet

--- Scape Town/Assets/_ScapeTown/Scripts/FadeReduceFOV.cs
+++ Scape Town/Assets/_ScapeTown/Scripts/FadeReduceFOV.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.PostProcessing;
 
 public class FadeReduceFOV : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private PostProcessVolume _ppv; 
+    [SerializeField] private XRIDefaultInputActions _ia; 
+
+    private void Start() {
+        if (_ppv != null)
+        {
+            Debug.Log("We don't have it boys");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() {
+        _ia.XRILeftHandLocomotion.Move.performed += ToggleOnVignette;
+        _ia.XRILeftHandLocomotion.Move.canceled += ToggleOffVignette;
+    }
+    private void OnDisable() {
+        _ia.XRILeftHandLocomotion.Move.performed -= ToggleOnVignette;
+        _ia.XRILeftHandLocomotion.Move.canceled -= ToggleOffVignette;
+    }
+
+    public void ToggleVignette()
     {
-        
+        _ppv.enabled = !_ppv.enabled;
+    }   
+    public void ToggleVignette(bool newValue)
+    {
+        _ppv.enabled = newValue;
+    }
+    public void ToggleOnVignette(InputAction.CallbackContext context)
+    {
+        ToggleVignette(true);
+    }
+    public void ToggleOffVignette(InputAction.CallbackContext context)
+    {
+        ToggleVignette(false);
     }
 }
