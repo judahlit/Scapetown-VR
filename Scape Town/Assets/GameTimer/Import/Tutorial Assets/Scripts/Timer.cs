6615b2a0927dd2828891f185b801efc78c0736cf
Joey van de Looverbosch <joeyvandelooverbosch@hotmail.com>
2022-06-10 10:34:21 +0200

    Fixed branch

---
+++ Scape Town/Assets/GameTimer/Import/Tutorial Assets/Scripts/Timer.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    private bool audioHasPlayed;
+    public AudioSource endOfTime;
+
+    [Header("Time Values")]
+    [Range(0,60)]
+    public int seconds;
+    [Range(0, 60)]
+    public int minutes;
+    [Range(0, 60)]
+    public int hours;
+
+    public Color fontColor;
+
+    public bool showMilliseconds;
+
+    private float currentSeconds;
+    private int timerDefault;
+
+    void Start()
+    {
+        // Global var for color of font
+        timerText.color = fontColor;
+        // Set default timer to 0
+        timerDefault = 0;
+        // Apply basic time, minute and hour logic to update the timer default value.
+        timerDefault += (seconds + (minutes * 60) + (hours * 60 * 60));
+        currentSeconds = timerDefault;
+    }
+
+    void Update()
+    {
+        // If time has ran out, execute the TimeUp method.
+        if((currentSeconds -= Time.deltaTime) <= 0)
+        {
+            TimeUp();
+        }
+        // Otherwise update the timer text with the correct value.
+        else
+        {
+            if(showMilliseconds)
+                timerText.text = TimeSpan.FromSeconds(currentSeconds).ToString(@"hh\:mm\:ss\:fff");
+            else
+                timerText.text = TimeSpan.FromSeconds(currentSeconds).ToString(@"hh\:mm\:ss");
+        }
+    }
+
+    private void TimeUp()
+    {
+        // Execute after time has run out.
+
+        Debug.Log("Time's up!");
+
+        if (audioHasPlayed == false)
+        {
+            endOfTime.Play();
+            audioHasPlayed = true;
+        }
+        
+
+        
+
+        if (showMilliseconds)
+            timerText.text = "00:00:00:000";
+        else
+            timerText.text = "00:00:00";
+    }
+}
